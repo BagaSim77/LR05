5a211874e1aeb8b38db82ba3278303c3dd23ef68
BagaSim77 <54947498+BagaSim77@users.noreply.github.com>
2019-12-17 01:26:31 +0300

    Add files via upload

---
+++ TestDish.java
@@ -0,0 +1,8 @@
+package Dish;
+
+public class TestDish {
+    public static void main(String[] args) {
+        Tarelka t = new Tarelka("стекло", "синий", "средняя");
+        System.out.println(t);
+    }
+}
